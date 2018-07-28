40808f0b2ce34f1e17b7070317d8adf86594eeb1
cloudpractise <825547517@qq.com>
2018-07-28 17:22:05 +0800

    Update find_regions_per_ami_id.py

--- find_regions_per_ami_id.py
+++ find_regions_per_ami_id.py
@@ -6,7 +6,6 @@ client = boto3.client('ec2')
 regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
 # define ami-id
 ami_id = sys.argv[1]
-
 # find the region name per ami id
 for i in regions:
     client = boto3.client('ec2', region_name = i)
@@ -16,3 +15,5 @@ for i in regions:
     if str(response) != '[]':
         print "region_name with ami_id is ", i
         break
+#usage 
+$python find-aws-region-by-ami-id ami_id
